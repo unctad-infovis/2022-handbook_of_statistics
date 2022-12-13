24b42ef7dc47eb437d70bd0e5fbc4f5d707c86ff
Teemo Tebest <teemo.tebest@gmail.com>
2022-12-13 15:51:04 +0100

    Updated

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -47,6 +47,7 @@ function App() {
                   <h2>
                     <br />
                     <br />
+                    <br />
                     <Typewriter
                       options={{
                         // delay: 'natural' // or milliseconds
@@ -55,17 +56,22 @@ function App() {
                       onInit={(typewriter) => {
                         typewriter.typeString('What are the major trends in ')
                           .pauseFor(50)
-                          .pasteString('<span class="highlight">international trade </span>')
+                          .pasteString('<span class="highlight">trade </span>')
                           .typeString(' and ')
                           .pauseFor(50)
-                          .pasteString('<span class="highlight">development</span>')
+                          .pasteString('<span class="highlight">development</span>?')
                           .typeString('')
                           .pauseFor(1500)
-                          .pasteString('<br /><br /><strong>UNCTAD’s<br /><span class="highlight">Handbook of Statistics</span><br />will tell you</strong>')
+                          .pasteString('<br /><br /><strong>UNCTAD’s<br /><span class="highlight">Handbook of Statistics 2022</span> has all the data.</strong>')
                           .start();
                       }}
                     />
                   </h2>
+                  <p>
+                    Browse the key messages or
+                    {' '}
+                    <a href="https://unctad.org/webflyer/handbook-statistics-2022" onClick={() => track('Click', 'webflyer_link')}>download the report</a>
+                  </p>
                 </div>
                 <div className="infograph_container">
                   <img className="infograph" src={`${(window.location.href.includes('unctad.org')) ? 'https://storage.unctad.org/2022-handbook_of_statistics/' : './'}assets/img/2022-handbook_of_statistics_infograph.svg`} alt="Infograph" />
@@ -99,11 +105,11 @@ function App() {
                     will slow in 2022
                   </h3>
                   <NumberAnimation labels={['2021', '2022']} prefix="+" suffix="%" values={[5.7, 3.3]} />
-                  <p>Nowcasts show that trade in both goods and services will slow down. Growth in merchandise exports is expected to decline by half.</p>
+                  <p>UNCTAD nowcasts show trade in goods and services will slow down. Growth in merchandise exports is expected to decline by half.</p>
                 </div>
                 <div className="bottom">
                   <a href="https://hbs.unctad.org/gross-domestic-product/" onClick={() => track('Click', 'card_2_link')} className="readmore" target="_blank" rel="noreferrer">Dive into the data</a>
-                  <div className="source">World’s real GDP in 2022 is based on UNCTAD nowcast</div>
+                  <div className="source">World’s real GDP in 2022 is based on UNCTAD nowcasts</div>
                 </div>
               </>
             )}
@@ -128,7 +134,7 @@ function App() {
                     {' '}
                     of developing economies increased
                   </h3>
-                  <p>The increase was mirrored by a widening trade deficit for developed economies. The surplus increased especially in Africa.</p>
+                  <p>The increase was mirrored by a widening trade deficit for developed economies. The surplus increased most in Africa.</p>
                 </div>
                 <div className="bottom">
                   <a href="https://hbs.unctad.org/total-merchandise-trade/" onClick={() => track('Click', 'card_3_link')} className="readmore" target="_blank" rel="noreferrer">Dive into the data</a>
@@ -157,12 +163,12 @@ function App() {
                     {' '}
                     increased but did not reach
                     {' '}
-                    <span className="nowrap">pre-COVID-19</span>
+                    <span className="nowrap">pre-pandemic</span>
                     {' '}
-                    level
+                    levels
                   </h3>
                   <NumberAnimation labels={['2020', '2021']} prefix="+" suffix="%" values={[-17.7, 17.2]} />
-                  <p>In 2022, UNCTAD nowcast suggests continued growth of +14.6%.</p>
+                  <p>In 2022, UNCTAD nowcasts show continued growth of +14.6%.</p>
                 </div>
                 <div className="bottom">
                   <a href="https://hbs.unctad.org/total-trade-in-services/" onClick={() => track('Click', 'card_4_link')} className="readmore" target="_blank" rel="noreferrer">Dive into the data</a>
@@ -189,12 +195,12 @@ function App() {
                   <h3>
                     <span className="highlight">Merchandise trade</span>
                     {' '}
-                    bounced back after
+                    bounced back from
                     {' '}
                     <span className="nowrap">COVID-19</span>
                   </h3>
                   <NumberAnimation labels={['2020', '2021']} prefix="+" suffix="%" values={[-7.2, 26.5]} />
-                  <p>The growth was largely driven by fuels, +72%. In 2022, UNCTAD nowcast suggests continued growth of 13.8%.</p>
+                  <p>The growth was largely driven by fuels (+72%). In 2022, UNCTAD nowcasts show continued growth of 13.8%.</p>
                 </div>
                 <div className="bottom">
                   <a href="https://hbs.unctad.org/total-merchandise-trade/" onClick={() => track('Click', 'card_5_link')} className="readmore" target="_blank" rel="noreferrer">Dive into the data</a>
@@ -225,7 +231,7 @@ function App() {
                     soared in 2021
                   </h3>
                   <div className="numbers_container"><div className="numbers_wrapper"><span className="number_value">{isVisible && (<CountUp easingFn={easingFn} start={0} delay={0.7} end={55} decimals={0} duration={4} separator="," useEasing prefix="+" suffix="%" />)}</span></div></div>
-                  <p>Fuels accounted for 22 percentage points of the growth. The upward trend continued this year with prices hitting their highest levels in nearly three decades.</p>
+                  <p>Fuels accounted for 22 percentage points of the growth. The upward trend continued this year, with prices hitting their highest levels in nearly three decades.</p>
                 </div>
                 <div className="bottom">
                   <a href="https://hbs.unctad.org/prices/" onClick={() => track('Click', 'card_6_link')} className="readmore" target="_blank" rel="noreferrer">Dive into the data</a>
@@ -251,13 +257,13 @@ function App() {
                 <div className={`content ${isVisible ? 'visible' : ''}`}>
                   <div className="icon_container"><img src={`${(window.location.href.includes('unctad.org')) ? 'https://storage.unctad.org/2022-handbook_of_statistics/' : './'}assets/img/2022-handbook_of_statistics_plane.svg`} alt="Commodities icon" /></div>
                   <h3>
-                    <span className="highlight">Trade in travel</span>
+                    <span className="highlight">International travel</span>
                     {' '}
-                    has not recover to
+                    has not recovered to
                     {' '}
-                    <span className="nowrap">pre-pandemic</span>
+                    <span className="nowrap">pre-pandemic levels</span>
                   </h3>
-                  <p>Travel exports were still less than half (42%) their pre-pandemic value of 2019.</p>
+                  <p>In 2021, travel exports were still less than half (42%) their value in 2019.</p>
                 </div>
                 <div className="bottom">
                   <a href="https://hbs.unctad.org/trade-in-services-by-category/" onClick={() => track('Click', 'card_7_link')} className="readmore" target="_blank" rel="noreferrer">Dive into the data</a>
@@ -341,14 +347,14 @@ function App() {
                 }
                 <div className={`content ${isVisible ? 'visible' : ''}`}>
                   <h3>
-                    And world’s population hit
+                    The world’s population hit
                     {' '}
                     <span className="highlight">8&nbsp;billion people</span>
                     {' '}
                     in November 2022
                   </h3>
                   <People />
-                  <p>But the growth has been declining since late 1980s. In 2021, it dipped down to +0.87% from +1.05% just in 2019.</p>
+                  <p>But the growth has been declining since the late 1980s. In 2021, it dipped to +0.87%, down from +1.05% in 2019.</p>
                 </div>
                 <div className="bottom">
                   <a href="https://hbs.unctad.org/total-and-urban-population/" onClick={() => track('Click', 'card_10_link')} className="readmore" target="_blank" rel="noreferrer">Dive into the data</a>
@@ -374,14 +380,12 @@ function App() {
                 <div className={`content ${isVisible ? 'visible' : ''}`}>
                   <div className="icon_container"><img src={`${(window.location.href.includes('unctad.org')) ? 'https://storage.unctad.org/2022-handbook_of_statistics/' : './'}assets/img/2022-handbook_of_statistics_computer.svg`} alt="Commodities icon" /></div>
                   <h3>
-                    <a href="https://hbs.unctad.org/">
-                      <div>See the e-version of the Handbook.</div>
-                      <div className="highlight">Get the data.</div>
-                      <div className="highlight">Interact with the maps and charts.</div>
-                    </a>
+                    <div>See the e-version of the Handbook.</div>
+                    <div className="highlight">Interact with the data.</div>
                   </h3>
-                  <a href="https://hbs.unctad.org/" onClick={() => track('Click', 'card_11_link')} className="readmore" target="_blank" rel="noreferrer">Handbook of Statistics 2022</a>
-                  <a href="https://www.youtube.com/watch?v=goQ_gEP3ihk" onClick={() => track('Click', 'video_link')} className="readmore" target="_blank" rel="noreferrer">Video</a>
+                  <a href="https://hbs.unctad.org/" onClick={() => track('Click', 'card_11_link')} className="readmore" target="_blank" rel="noreferrer">e-Handbook of Statistics 2022</a>
+                  <a href="https://unctad.org/webflyer/handbook-statistics-2022" onClick={() => track('Click', 'webflyer_link')} className="readmore" target="_blank" rel="noreferrer">Download the report (pdf)</a>
+                  <a href="https://www.youtube.com/watch?v=goQ_gEP3ihk" onClick={() => track('Click', 'video_link')} className="readmore" target="_blank" rel="noreferrer">Watch the video</a>
                 </div>
               </>
             )}
